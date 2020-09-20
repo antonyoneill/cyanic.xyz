c6c723b1cdb09951d2f862e74385a01cf31bbf20
Antony O'Neill <antony@ant-web.co.uk>
2020-09-20 17:56:40 +0100

    Add mail DNS records

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,15 +6,64 @@ resource "hetznerdns_zone" "default" {
 resource "hetznerdns_record" "root" {
   zone_id = hetznerdns_zone.default.id
   name = "@"
-  value = hcloud_floating_ip.main.ip_address
   type = "A"
+  value = hcloud_floating_ip.main.ip_address
   ttl = 60
 }
 
 resource "hetznerdns_record" "web" {
   zone_id = hetznerdns_zone.default.id
   name = "www"
-  value = hcloud_floating_ip.main.ip_address
   type = "A"
+  value = hcloud_floating_ip.main.ip_address
   ttl = 60
 }
+
+resource "hetznerdns_record" "mail" {
+  zone_id = hetznerdns_zone.default.id
+  name = "mail"
+  type = "A"
+  value = hcloud_floating_ip.main.ip_address
+  ttl = 60
+}
+
+resource "hetznerdns_record" "autodiscover" {
+  zone_id = hetznerdns_zone.default.id
+  name = "autodiscover"
+  type = "CNAME"
+  value = "mail"
+  ttl = 60
+}
+
+resource "hetznerdns_record" "autoconfig" {
+  zone_id = hetznerdns_zone.default.id
+  name = "autoconfig"
+  type = "CNAME"
+  value = "mail"
+  ttl = 60
+}
+
+resource "hetznerdns_record" "mx" {
+  zone_id = hetznerdns_zone.default.id
+  name = "@"
+  type = "MX"
+  value = "10 mail"
+  ttl = 60
+}
+
+resource "hetznerdns_record" "spf" {
+  zone_id = hetznerdns_zone.default.id
+  name = "@"
+  type = "TXT"
+  value = "v=spf1 mx a -all"
+  ttl = 60
+}
+
+resource "hetznerdns_record" "dmarc" {
+  zone_id = hetznerdns_zone.default.id
+  name = "_dmarc"
+  type = "TXT"
+  value = "v=DMARC1; p=reject; rua=mailto:mailauth-reports@cyanic.xyz"
+  ttl = 60
+}
+
