a1a88f55427aa09ded2a81f3ca40c3f628c6990c
Antony O'Neill <antony@ant-web.co.uk>
2020-09-20 16:34:22 +0100

    Add root DNS A record

--- dns.tf
+++ dns.tf
@@ -3,6 +3,14 @@ resource "hetznerdns_zone" "default" {
   ttl = 3600
 }
 
+resource "hetznerdns_record" "root" {
+  zone_id = hetznerdns_zone.default.id
+  name = "@"
+  value = hcloud_floating_ip.main.ip_address
+  type = "A"
+  ttl = 60
+}
+
 resource "hetznerdns_record" "web" {
   zone_id = hetznerdns_zone.default.id
   name = "www"
